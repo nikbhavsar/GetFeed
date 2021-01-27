c070b43dbec9dcbec131425167e4a2b9c10850e6
Nikhar <bhavsarnikhar21@gmail.com>
2021-01-27 00:39:53 -0800

    database config
    configure MongoDB drivers into our app

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://nikhar:nikharHatchways@getfeeds.zhyfw.mongodb.net/test?retryWrites=true&w=majority',
+};
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB successfully connected'))
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('API Running'));
 
